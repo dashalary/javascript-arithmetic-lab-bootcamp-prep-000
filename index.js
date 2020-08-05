c6fda2eab6127bd07b8b7b88deea79e9832794a6
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-05 21:38:08 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function add() {
+  return 1 + 80
+}
+
+function subtract () {
+  return 60 - 40
+}
+
+function multiply() {
+  return 2 * 3.4
+}
+
+function divide () {
+  return 5.0 / 2.5
+}
+
+function increment(n) {
+  return n + 1
+}
+
+function decrement(n) {
+  return n - 1
+}
+
